261fcb87b84b7a4d56e18a60df8e0bd30052fd8a
Matheus Muniz <148823772+Muniz17@users.noreply.github.com>
2024-09-17 20:01:45 -0300

    Add files via upload

---
+++ Condicionais.java/src/ExemploForArrays.java
@@ -0,0 +1,9 @@
+public class ExemploForArrays {
+    public static void main(String[] args) {
+        String alunos [] = {"Felipe", "Jonas", "Julia", "Marcos"};
+        for(int x = 0; x < alunos.length; x++) {
+
+            System.out.println("O aluno on indice x= " + x + " é " + alunos [ x ]);
+        }
+    }
+}
---
+++ Condicionais.java/src/resultadoEscolar.java
@@ -0,0 +1,18 @@
+public class resultadoEscolar {
+    public static void main(String[] args) {
+    int nota = 4;
+        if(nota >= 7) 
+            System.out.println("APROVADO");
+
+        else if(nota >= 5 && nota < 7 )
+            System.out.println("Recuperação");
+
+
+        else
+            System.out.println("REPROVADO");
+    
+    
+    }
+
+
+}
